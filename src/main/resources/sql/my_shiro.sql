507aab418a376f7ad83e248b4eb9171f45736e43
pengpan <13294158776@163.com>
2020-01-06 15:35:27 +0800

    数据表

---
+++ src/main/resources/sql/my_shiro.sql
@@ -0,0 +1,98 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.1.62-community : Database - my_shiro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`my_shiro` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `my_shiro`;
+
+/*Table structure for table `sys_menu` */
+
+DROP TABLE IF EXISTS `sys_menu`;
+
+CREATE TABLE `sys_menu` (
+  `menu_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '权限ID',
+  `name` varchar(50) DEFAULT NULL COMMENT '权限名称',
+  `perms` varchar(500) DEFAULT NULL COMMENT '权限标识',
+  PRIMARY KEY (`menu_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='权限表';
+
+/*Data for the table `sys_menu` */
+
+insert  into `sys_menu`(`menu_id`,`name`,`perms`) values (1,'查看用户列表','sys:user:info'),(2,'查看角色列表','sys:role:info'),(3,'查看权限列表','sys:menu:info'),(4,'查看所有数据','sys:info:all');
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `role_id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '角色ID',
+  `role_name` varchar(50) NOT NULL COMMENT '角色名称',
+  PRIMARY KEY (`role_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='角色表';
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`role_id`,`role_name`) values (1,'ADMIN'),(2,'USER');
+
+/*Table structure for table `sys_role_menu` */
+
+DROP TABLE IF EXISTS `sys_role_menu`;
+
+CREATE TABLE `sys_role_menu` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `role_id` bigint(11) DEFAULT NULL COMMENT '角色ID',
+  `menu_id` bigint(11) DEFAULT NULL COMMENT '权限ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='角色与权限关系表';
+
+/*Data for the table `sys_role_menu` */
+
+insert  into `sys_role_menu`(`id`,`role_id`,`menu_id`) values (1,1,1),(2,1,2),(3,1,3),(4,2,1),(5,1,4);
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `user_id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(100) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(50) DEFAULT NULL COMMENT '盐值',
+  `state` varchar(50) DEFAULT NULL COMMENT '状态:NORMAL正常  PROHIBIT禁用',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='系统用户表';
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`user_id`,`username`,`password`,`salt`,`state`) values (1,'admin','a1bb09ad5dea12e0f94762cb116c447e80c784d8aa2c6625263f7f3436cdd583','RvP3UID2n30Q2sycZYvH','NORMAL'),(2,'user','376eb5d2698c804ee83594fe8b0217f03ad138a046f7fa42b44c232c2e5e2b38','OVlrD37bDUKNcFRB10qG','NORMAL');
+
+/*Table structure for table `sys_user_role` */
+
+DROP TABLE IF EXISTS `sys_user_role`;
+
+CREATE TABLE `sys_user_role` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `user_id` bigint(11) DEFAULT NULL COMMENT '用户ID',
+  `role_id` bigint(11) DEFAULT NULL COMMENT '角色ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='用户与角色关系表';
+
+/*Data for the table `sys_user_role` */
+
+insert  into `sys_user_role`(`id`,`user_id`,`role_id`) values (1,1,1),(2,2,2);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
